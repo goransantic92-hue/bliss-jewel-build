6e5472730827d2e913b7a73470cdd2fecc8ffe30
goransantic92-hue <goransantic92@gmail.com>
2026-03-29 20:59:56 +0200

    top

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const MainLayout = () => (
   <div className="min-h-screen bg-background flex flex-col">
+    <ScrollToTop />
     <Navbar />
     <main className="flex-1">
       <Outlet />
